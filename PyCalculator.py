e15eebb960e4ab2eb76c818fbf933e3e9f172253
Steffen P <steffen.pretzel@gmx.de>
2023-03-04 22:22:09 +0100

    Initial commit

--- PyCalculator.py
+++ PyCalculator.py
@@ -1,1 +1,13 @@
 # This is the calculator project source code.
+
+
+print("Which mathematical operation do you want to perform?")
+print("Print 'A' for addition.")
+print("Print 'S' for subtraction.")
+print("Print 'M' for multiplication.")
+print("Print 'D' for division.")
+choice = input("Your choice: ").lower()
+
+
+num1 = float(input("Enter num1: "))
+num2 = float(input("Enter num2: "))
